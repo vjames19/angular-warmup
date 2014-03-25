a5fb2565d036184283cd72f288831bdc819cce28
Victor J. Reventos <victor.reventos1@gmail.com>
2014-03-24 21:06:52 -0400

    Mere mortal html page.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,3 @@
 'use strict';
 
-angular.module('angularWarmupApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('angularWarmupApp', []);
